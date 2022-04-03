16b74cb12fe5242c24080a9b8ae228a0c4b2b8a8
romameg <roma.megrelishvili13@gmail.com>
2022-04-03 04:00:00 -0700

    created map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,36 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React from 'react';
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import './App.css';
+import Map, {FullscreenControl,Popup, GeolocateControl} from 'react-map-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZG9naWJvaTE1IiwiYSI6ImNsMWo0Zjk4aTB0NGQzb281OG1nMDcycm0ifQ.9CGpm27pBuSCauqVrzwLsQ';
 
 
 function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-    
-  });
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
 
   return (
-    <div>
-<div className="sidebar">
-Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-</div>
-<div ref={mapContainer} className="map-container" />
-</div>
+   
+      <Map
+        initialViewState={{
+          latitude: 40,
+          longitude: -100,
+          zoom: 3.5,
+          bearing: 0,
+          pitch: 0
+        }}
+        style={{width: 600, height: 400}}
+
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxAccessToken="pk.eyJ1IjoiZG9naWJvaTE1IiwiYSI6ImNsMWo0Zjk4aTB0NGQzb281OG1nMDcycm0ifQ.9CGpm27pBuSCauqVrzwLsQ"
+      >
+           <FullscreenControl position="top-left" />
+           <GeolocateControl position='top-right'/>
+    
+       
+
+      
+      </Map>
+  
   );
 }
 
